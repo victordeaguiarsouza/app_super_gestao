1fcd3b12a52d24c8c2e2b3422f9859e9bc4cd708
victor <victordeaguiarsouza@gmail.com>
2021-08-03 17:29:11 -0300

    criaçao das tabelas: produtos, produto_filiais, unidades, filiais

--- database/migrations/2021_07_29_194657_create_fornecedores_table.php
+++ database/migrations/2021_07_29_194657_create_fornecedores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSiteContatosTable extends Migration
+class CreateFornecedoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateSiteContatosTable extends Migration
      */
     public function up()
     {
-        Schema::create('site_contatos', function (Blueprint $table) {
+        Schema::create('fornecedores', function (Blueprint $table) {
             $table->id();
-            $table->string('nome'     , 50);
-            $table->string('telefone' , 20);
-            $table->string('email'    , 80);
-            $table->integer('motivo');
-            $table->text('mensagem');
+            $table->string('nome', 50);
             $table->timestamps();
         });
     }
@@ -28,10 +24,9 @@ class CreateSiteContatosTable extends Migration
      * Reverse the migrations.
      *
      * @return void
-    */
+     */
     public function down()
     {
-        Schema::dropIfExists('site_contatos');
+        Schema::dropIfExists('fornecedores');
     }
-
 }
